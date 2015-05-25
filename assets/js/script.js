d2eb8bc11c1226b953532248dc4e0e07dc3cb07d
huibiaoli <huibiaoli@gmail.com>
2015-05-26 01:06:06 +0800

    修改tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,9 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.程序'),
+    tag3       = $('.数据库'),
+    tag4       = $('.架构');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
@@ -30,7 +28,7 @@ var clickHandler = function(k) {
     window['tag'+k].delay(50).fadeIn(350);
   }
 };
-for (var i = 1; i <= 6; i++) {
+for (var i = 1; i <= 4; i++) {
   $('#js-label' + i).on('click', clickHandler(i));
 }
 
