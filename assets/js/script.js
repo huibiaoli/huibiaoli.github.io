c2588b70169cb11e7ef0891b398505e63ffe729f
huibiaoli <huibiaoli@gmail.com>
2015-05-26 01:39:27 +0800

    显示评论

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,7 +132,7 @@ function afterPjax() {
   var ds_loaded = false,
       top = $('#disqus_thread').offset().top;
       identifier = $('#post__title').data('identifier');
-  window.disqus_shortname = '';
+  window.disqus_shortname = 'huibiaoli';
   window.disqus_identifier = identifier;
 
   function check() {
